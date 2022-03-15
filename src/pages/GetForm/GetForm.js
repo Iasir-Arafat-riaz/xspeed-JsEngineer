f096b9511878b2efccb32425bb3efb895ee6a38f
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-03-15 07:31:26 +0600

    form finalize

--- src/pages/GetForm/GetForm.js
+++ src/pages/GetForm/GetForm.js
@@ -2,49 +2,46 @@ import React from "react";
 import { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
-import "./GetForm.css"
+import "./GetForm.css";
 
 const GetForm = () => {
-    const submitForm=(e)=>{
-        e.preventDefault()
-console.log(formRef.current.value);
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log(formRef.current.value);
+  };
 
-    }
+  const { register, handleSubmit, reset, watch } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
-    const { register, handleSubmit,reset,watch } = useForm();
-  const onSubmit = data => {
-      console.log(data)
-    };
-
-    const formRef = useRef()
+  const formRef = useRef();
   return (
     <div className="tableForm">
-     <div>
-     <h1 className="formText">Get form</h1>
-      <br />
-      <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName")} />
-      <input {...register(`email`)} />
-      <textarea name="" id="" cols="30" rows="3"></textarea>
+      <div>
+        <h1 className="formText">Get form</h1>
+        <br />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register("firstName")} />
+          <input  {...register(`email`)} />
+          <textarea  name="" id="" cols="30" rows="3" {...register("text")} />
 
-      <select {...register("gender")}>
-        <option value="female">female</option>
-        <option value="male">male</option>
-        <option value="other">other</option>
-      </select>
-      {/* <input  /> */}
-      <div class=" text-center ">
-      <Button  class="btn btn-info"  type="submit"> Submit</Button>
-        </div>
-     
-    </form>
-     </div>
-
-      
+          <select {...register("gender")}>
+            <option value="female">female</option>
+            <option value="male">male</option>
+            <option value="other">other</option>
+          </select>
+          {/* <input  /> */}
+          <div class=" text-center ">
+            <Button class="btn btn-info" type="submit">
+              {" "}
+              Submit
+            </Button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
-
-
 
 export default GetForm;
